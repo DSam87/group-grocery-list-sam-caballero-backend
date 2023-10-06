df14b3d89c0095f985453f61265e61ff14fcb540
Sam <deleonsam87@gmail.com>
2023-10-05 22:41:51 -0500

    add missing origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   "https://group-grocery-list-sam-caballero.onrender.com/",
   "https://group-grocery-list-sam-caballero.onrender.com",
+  "https://group-grocery-list-sam-caballero.onrender.com/auth",
 ];
 
 module.exports = allowedOrigins;
